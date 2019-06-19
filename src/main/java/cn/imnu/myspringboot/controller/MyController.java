ba6f40e2d82595cfaded38f6231f818956b03a64
1298743328szyasy <1298743328@qq.com>
2019-06-19 15:33:40 +0800

    熟悉线上和开发两种模式

--- src/main/java/cn/imnu/myspringboot/controller/MyController.java
+++ src/main/java/cn/imnu/myspringboot/controller/MyController.java
@@ -1,14 +1,29 @@
 package cn.imnu.myspringboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MyController {
+    @Value("${mail.config.name}")
+    private String name;
+    @Value("${mail.config.description}")
+    private String description;
+    @Value("${mail.config.hot-sales}")
+    private Integer hotSales;
+    @Value("${mail.config.show-advert}")
+    private Boolean showAdvert;
     @RequestMapping("/out")
     @ResponseBody
     public String out(){
         return "success";
+    }
+    @RequestMapping("/info")
+    @ResponseBody
+    public String info(){
+        return String.format("name:%s,description:%s,hot-sales:%s,show-advert:%s",
+                name,description,hotSales,showAdvert);
     }
 }
